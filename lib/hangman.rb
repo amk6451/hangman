b37b2d022ba54f0c480310a97b34e6f56a207f9d
Alan Kwong <akwong910@gmail.com>
2023-05-13 09:23:59 -0400

    Added hangman_counter and compare_set for win/lose conditions

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -7,7 +7,10 @@ class Game
 def initialize()
   @secret_word = pick_random_line()
   @guess_word = Array.new(@secret_word.length,"_")
-  @chosen_letters = Set.new()
+  
+  @secret_word_set = Set.new(@secret_word.split(//))
+  @chosen_letters_set = Set.new()
+  @count = 0
 end  
 
 def display_guesses()
@@ -15,7 +18,17 @@ def display_guesses()
 end
 
 def chosen_letters()
-  p @chosen_letters.join(//)
+  p @chosen_letters_set.to_a
+end
+
+
+def compare_set()
+  #check of all letters of the secret word are included in our set of guess letters
+  if @secret_word_set.subset?(@chosen_letters_set)
+    puts "The secret word #{@secret_word}"
+    return true
+  end
+  return false
 end
 
 def pick_letters()
@@ -23,22 +36,32 @@ def pick_letters()
   puts "please give a valid letter" + "\n"
   letter = gets.chomp
   #function runs again if letter already chosen, more than 1 letter, or not character not from alphabet
-  if @chosen_letters.include?(letter) || letter.length != 1 || /^[a-zA-Z]$/.match(letter) == nil
+  if @chosen_letters_set.include?(letter) || letter.length != 1 || /^[a-zA-Z]$/.match(letter) == nil
     pick_letters
   else
     #adds lowercase letter to set of letters chosen
-    @chosen_letters.add(letter.downcase)
-    p @chosen_letters
+    return letter.downcase
+    # @chosen_letters.add(letter.downcase)
   end
 end
 
 def update_blanks()
   #checks the players set of chosen letters against the "secret word"
-  @secret_word.each_with_index do |letter, index|
+  @secret_word.split(//).each_with_index do |letter, index|
     #when a matched letter is found, the guessed word is updated
-    if @chosen_letters.include?(letter)
+    if @chosen_letters_set.include?(letter)
       @guess_word[index] = letter
     end
+  end
+end
+
+def hang_counter(selected)
+  @chosen_letters_set.add(selected)
+  if @secret_word_set.include?(selected)
+    return @count
+  else
+    puts "Incorrect selection, number of bad counts is #{@count+1}"
+    return @count += 1
   end
 end
 
@@ -47,22 +70,33 @@ def pick_random_line()
     #randomly select a word between 5 and 12 characters long
     selection = $lines[rand($lines.length)].chomp
     if selection.length > 5 && selection.length < 12
-      #returns an array of the string
-      return selection.split(//)
+      #returns secret word
+      return selection
     else
       pick_random_line()
     end
   end
-
-
 end
 
 x = Game.new()
 
 while true
   x.display_guesses()
-  x.pick_letters()
-  x.update_blanks()
+  value = x.pick_letters()
 
+  #if more than 6 guess, game ends
+  if x.hang_counter(value) > 6
+    puts "you lose, you had too many guesses"
+    break
+  end
+  
+  x.update_blanks()
+  x.chosen_letters()
+
+  #true means all secret letters were correctly guessed correctly
+  if x.compare_set() == true
+    puts "game is finished, you win"
+    break
+  end
 end
 
